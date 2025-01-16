46d81fbbc4db4ffad85f67dd559afc32b7efc8bb
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2025-01-16 16:06:22 +0200

    add in models.py class Schema, Column

--- schemas/models.py
+++ schemas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Schema(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="schemas")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Column(models.Model):
+    DATA_TYPES = [
+        ("string", "String"),
+        ("integer", "Integer"),
+        ("date", "Date"),
+    ]
+
+    name = models.CharField(max_length=255)
+    date_type = models.CharField(max_length=50, choices= DATA_TYPES)
+    schema = models.ForeignKey(Schema, on_delete=models.CASCADE, related_name="columns")
