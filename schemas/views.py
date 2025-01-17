eae0648776520f3b3c9a460127ca208b3d639673
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2025-01-17 14:56:18 +0200

    add to views.py def create_schema/list_schemas

--- schemas/views.py
+++ schemas/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from.forms import SchemasForm, ColumnForm
+from .models import Schema, Column
 
-# Create your views here.
+
+@login_required
+def create_schema(request):
+    if request.method == "POST":
+        schema_form = SchemasForm(request.POST)
+        if schema_form.is_valid():
+            schema = schema_form.save(commit=False)
+            schema.user = request.user
+            schema.save()
+            return redirect("list_schemas")
+    else:
+        schema_form = SchemasForm
+    return render(request, "create_schema.html", {"form": schema_form})
+
+@login_required
+def list_schemas(request):
+    schemas = Schema.object.filter(user=request.user)
+    return render(request, "list_schemas.html", {"schemas": schemas})
