8ca62d702a35315ecc4b30db9982858f40cb5552
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2025-01-18 14:01:59 +0200

    add in models.py model GeneretedData and modified DataSchema/DataColumn

--- schemas/models.py
+++ schemas/models.py
@@ -2,19 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Schema(models.Model):
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="schemas")
+class DataSchema(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
-class Column(models.Model):
+
+class DataColumn(models.Model):
     DATA_TYPES = [
+        ("char", "Character"),
         ("string", "String"),
         ("integer", "Integer"),
         ("date", "Date"),
     ]
 
-    name = models.CharField(max_length=255)
-    date_type = models.CharField(max_length=50, choices= DATA_TYPES)
-    schema = models.ForeignKey(Schema, on_delete=models.CASCADE, related_name="columns")
+    schema = models.ForeignKey(DataSchema, on_delete=models.CASCADE, related_name="columns")
+    name = models.CharField(max_length=100)
+    date_type = models.CharField(max_length=10, choices= DATA_TYPES)
+
+    def __str__(self):
+        return f"{self.name} ({self.date_type})"
+
+
+class GeneratedData(models.Model):
+    schema = models.ForeignKey(DataSchema, on_delete=models.CASCADE)
+    data = models.JSONField()
+
+    def __str__(self):
+        return f"Generated Data for {self.schema.name}"
+
