8c67fec891c794f368452218f830a8f5a2b8dcfc
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2025-01-20 16:00:05 +0200

    modified admin.py

--- schemas/admin.py
+++ schemas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import DataSchema, DataColumn, GeneratedData
 
 
-admin.site.regisret(DataSchema)
+admin.site.register(DataSchema)
 admin.site.register(DataColumn)
 admin.site.register(GeneratedData)
 
