8aad2149472ed92d5025d31318261a28bb38de42
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2025-01-20 15:11:43 +0200

    modified fake_csv_generator/urls.py

--- fake_csv_generator/urls.py
+++ fake_csv_generator/urls.py
@@ -22,6 +22,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("schemas/", include("schemas.urls")),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
