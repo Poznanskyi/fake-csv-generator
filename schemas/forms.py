37cbe6ba4169c8879ecd01ccb7206595b064ecf9
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2025-01-24 19:05:27 +0200

    modified schemas/forms.py class DataColumnForm

--- schemas/forms.py
+++ schemas/forms.py
@@ -12,3 +12,5 @@ class DataColumnForm(forms.ModelForm):
     class Meta:
         model = DataColumn
         fields = ["name", "data_type"]
+
+    date_type = forms.ChoiceField(choices=DataColumn.DATA_TYPES)
